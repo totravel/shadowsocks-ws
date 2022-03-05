f6427a1accbbcd9a0cc1bf29ee35d2340ad5311f
totravel <totravel@dr.com>
2022-03-06 00:11:32 +0800

    fix typo

--- ruleset.sh
+++ ruleset.sh
@@ -19,14 +19,19 @@ files=(
 
 url=https://raw.githubusercontent.com/Loyalsoldier/clash-rules/release/
 
+if [ ! -d ~/.config/clash ]; then
+  echo requires clash installed.
+  exit 1
+fi
+
 if [ ! -d ~/.config/clash/ruleset ]; then
   mkdir ~/.config/clash/ruleset
 fi
 
 for file in ${files[@]}; do
-  echo dowloading ${file}.yaml...
+  echo downloading ${file}.yaml...
   curl ${url}${file}.txt \
-    -s -k \
+    -s -S -k \
     -x socks5h://127.0.0.1:7890 \
     -o ~/.config/clash/ruleset/${file}.yaml
 done
