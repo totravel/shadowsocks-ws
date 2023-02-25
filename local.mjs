d9ce51562af5e2afd701e7a74556e089de242aca
totravel <totravel@foxmail.com>
2023-02-25 16:53:48 +0800

    support multiple ip

--- local.mjs
+++ local.mjs
@@ -112,7 +112,7 @@ const options = {
   timeout: config.timeout,
   origin: homeUrl.origin,
   headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
     'Accept-Encoding': 'gzip, deflate, br'
@@ -126,8 +126,8 @@ if (isIP(serverUrl.hostname)) {
   options.headers.Host = serverUrl.hostname
   options.servername = serverUrl.hostname // for SNI (Server Name Indication) TLS extension
 
-  if (isIP(config.server_address)) {
-    address.push(config.server_address)
+  if (config.server_address) {
+    address = config.server_address
   } else {
     try {
       infolog(`resolving ${serverUrl.hostname}...`)
